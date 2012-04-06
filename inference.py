302e0608c1ae28adb57c377051198dc7e85daf20
Kui Tang <kuitang@gmail.com>
2012-04-06 16:45:45 -0400

    added refs.bib

---
+++ inference.py
@@ -0,0 +1,9 @@
+import numpy as np
+from models import Blog, Document
+
+class SubjLDA(object):
+    def __init__(self): pass
+    
+    def train(self, blog, alpha, beta, gamma, max_iter=800): pass
+
+class HMM(object): pass
